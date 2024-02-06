9110112ced2cee102dce091b123d0ebf1aea137a
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-02-06 16:19:01 +0100

    Interfaces améliorées, interface principale réalisée

--- src/main/java/com/example/todolist/InterfacePrincipale.java
+++ src/main/java/com/example/todolist/InterfacePrincipale.java
@@ -1,4 +1,30 @@
 package com.example.todolist;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.w3c.dom.Text;
+
 public class InterfacePrincipale {
+
+    @FXML
+    public Button Addnew;
+    @FXML
+    public Button All;
+    @FXML
+    public Button Todo;
+    @FXML
+    public Button completed;
+
+    public void ajoutertaches(ActionEvent actionEvent) {
+    }
+
+    public void toutafficher(ActionEvent actionEvent) {
+    }
+
+    public void afficherTodo(ActionEvent actionEvent) {
+    }
+
+    public void affichertachescompletees(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/example/todolist/MainTodo.java
+++ src/main/java/com/example/todolist/MainTodo.java
@@ -10,14 +10,14 @@ public class MainTodo extends Application {
 
     private final int width = 800;
     private final int height = 500;
-
+    //, 300, 275
 
 
     @Override
     public void start(Stage Primarystage) {
         try {
             FXMLLoader loader = new FXMLLoader(MainTodo.class.getResource("interface_connexion.fxml"));
-            Scene scene = new Scene(loader.load(), 300, 275);
+            Scene scene = new Scene(loader.load());
             Primarystage.setScene(scene);
         } catch (Exception e) {
             System.out.println("Erreur:" + e.getMessage());
